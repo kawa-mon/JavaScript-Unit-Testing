fa25514820bbb7510c12eb14784dd74a7ac454b1
川島慎悟 <kawashingo1990@gmail.com>
2022-06-18 21:21:10 +0900

    Defining Behaviors & Fixing Errors In Your Code

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -15,3 +15,25 @@ it('should summarize all number values in an array', () => {
   )
   expect(result).toBe(expectedResult)
 })
+
+it('should yield NaN if a least one invalid number is provided', () => {
+  const inputs = ['invalid', 1]
+
+  const result = add(inputs)
+
+  expect(result).toBeNaN()
+})
+
+it('should yield a correct sum if an array of numeric string values is provided', () => {
+  const numbers = ['1', '2']
+
+  // Act
+  const result = add(numbers)
+
+  // Assert
+  const expectedResult = numbers.reduce(
+    (prevValue, curValue) => +prevValue + +curValue,
+    0
+  )
+  expect(result).toBe(expectedResult)
+})
