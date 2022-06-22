7850a0c41293e6009d7391ea5cabfbb9b811b131
川島慎悟 <kawashingo1990@gmail.com>
2022-06-22 11:15:42 +0900

    Testing Asynchronous Code With Callbacks (Wrong test is failed !!)

--- section6/async/async-example.test.js
+++ section6/async/async-example.test.js
@@ -2,11 +2,16 @@ import { expect } from 'chai'
 import { it } from 'vitest'
 import { generateToken } from './async-example'
 
-it('should generate a token', () => {
+it('should generate a token', (done) => {
   const testUserEmail = 'test@test.com'
 
-  generateToken(testUserEmail, (err, token) => {
-    // expect(token).toBeDefined()
-    expect(token).toBe(2)
+  generateToken(testUserEmail, (_err, token) => {
+    try {
+      expect(token).toBeDefined()
+      // expect(token).toBe(2)
+      done()
+    } catch (error) {
+      done(error)
+    }
   })
 })
